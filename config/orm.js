51d2a7264346f77aa3dba9a8f73fb0c5b0ec8551
Luz Mashas <luzmashas@Luzs-MacBook-Pro.local>
2018-02-12 10:29:30 -0500

    Finish configuration of orm file

--- config/orm.js
+++ config/orm.js
@@ -2,10 +2,78 @@ var connection = require("./connection.js");
 
 // Object Relational Mapper (ORM)
 
-//Methods Require selectAll()
-//Method Require InsertOne()
-//Method Require updateOne()
+// helper function for ? values in mysql syntax
+function printQuestionMarks(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
 
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(cols) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Bacon Cheeseburger => 'Bacon Cheeseburger')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {burger_name: 'Bacon Cheeseburger'} => ["burger_name='Bacon Cheeseburger'"]
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
+var orm = {
+  selectAll: function (table_name,cb){
+    var queryString = "SELECT * FROM " + table_name + ";";
+    connection.query(queryString,function(err,result){
+      if (err){
+        console.log("Error Message:  " + err);
+      }
+      cb(result);
+    });
+  },
+  InsertOne: function(table_name,cols,values,cb){
+    var queryString="INSER INTO " + table;
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString,values,function(err,result){
+          if (err){
+            console.log("Error Message:  " + err);
+          }
+          cb(result);
+    });
+  },
+
+  updateOne: function(table_name,cols,value,condition){
+        var queryString = "UPDATE " + table_name;
+        queryString += " SET ";
+        queryString += objToSql(cols);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    });
+  }
+};
 
 
 
