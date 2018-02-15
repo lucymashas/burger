4c58ebba563a6d8b885757f9c5f76993477391e4
Luz Mashas <luzmashas@Luzs-MacBook-Pro.local>
2018-02-15 12:55:02 -0500

    updates

--- public/assets/js/burgerEntry.js
+++ public/assets/js/burgerEntry.js
@@ -1,13 +1,13 @@
 $(function(){
-  $("#formName").on("submit",function(event){
+  $(".formName").on("submit",function(event){
     event.preventDefault();
     var hamburgerName = {
        name: $('#burgerName').val().trim(),
        devouredit: value = 1
    };
    $.ajax("/burgers",{
-     type:"POST",
-     data:hamburgerName
+     type: "POST",
+     data: hamburgerName
    }).then(
      function(){
        console.log("created New burger:");
